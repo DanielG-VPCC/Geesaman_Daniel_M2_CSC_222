205d900fd5583612c5bf6bbd6138a713175def06
Daniel <Daniel@GERALD>
2025-04-19 15:36:29 -0400

    wip

--- Geesaman_Daniel_M2_CSC_222.cpp
+++ Geesaman_Daniel_M2_CSC_222.cpp
@@ -2,9 +2,42 @@
 //
 
 #include <iostream>
+using namespace std;
+
+int sizeArr = 10;
+int const arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+
+int *reverseArray(int const arr[], int sizeArr);
+void print(int const arr[], int sizeArr);
+
+int *reverseArray(int const arr[], int sizeArr)
+{
+    int* revArr = new int[sizeArr];
+    int j = sizeArr;
+    for (int i = 0; i < sizeArr; i++)
+    {
+        j--;
+        revArr[i] = arr[j];
+    }
+    for (int i = 0; i < sizeArr; i++)
+    {
+        arr[i] = revArr[i];
+    }
+
+    return revArr;
+}
+
+void print(int const arr[], int sizeArr)
+{
+    for (int i = 0; i < sizeArr; i++)
+    {
+        cout << arr[i];
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int returnFcn = *reverseArray(arr, sizeArr);
+    print(arr, sizeArr);
 }
 
