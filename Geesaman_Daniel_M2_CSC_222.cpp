1f21a139a488268a9ed129c18465813d617da968
Daniel <Daniel@GERALD>
2025-04-19 17:02:12 -0400

    reverse array program figured out and completed

--- Geesaman_Daniel_M2_CSC_222.cpp
+++ Geesaman_Daniel_M2_CSC_222.cpp
@@ -1,5 +1,5 @@
 // Geesaman_Daniel_M2_CSC_222.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//This program reverses the constant array created at the inception of the program, and prints the output.
 
 #include <iostream>
 using namespace std;
@@ -7,10 +7,18 @@ using namespace std;
 int sizeArr = 10;
 int const arr[] = { 1,2,3,4,5,6,7,8,9,10 };
 
-int *reverseArray(int const arr[], int sizeArr);
+//these are my function prototypes
+int* reverseArray(int const arr[], int sizeArr);
 void print(int const arr[], int sizeArr);
 
-int *reverseArray(int const arr[], int sizeArr)
+/*This function accepts my defined constant array and the variable for the size of the array.
+
+The preconditions are that the constant array must be created, and that its size must be greater than 0.
+
+The postcondition is that the constant array has had its contents copied in reverse order to a dynamically created array,
+the function then returns a pointer to the newly reversed array.
+*/
+int* reverseArray(int const arr[], int sizeArr)
 {
     int* revArr = new int[sizeArr];
     int j = sizeArr;
@@ -19,25 +27,30 @@ int *reverseArray(int const arr[], int sizeArr)
         j--;
         revArr[i] = arr[j];
     }
-    for (int i = 0; i < sizeArr; i++)
-    {
-        arr[i] = revArr[i];
-    }
-
+    
     return revArr;
 }
 
+/*
+This function recieves the pointer to the reversed array created in the reverseArray function as well as the variable stating 
+the length of the original array.
+
+The preconditions of this function are that the pointer from the reversedArray function must have been created and passed into this print function,
+and the length of the sizeArr variable must be greater than 0.
+
+The postcondition of this function is that the reversed array will be printed to the screen.
+*/
 void print(int const arr[], int sizeArr)
 {
     for (int i = 0; i < sizeArr; i++)
     {
-        cout << arr[i];
+        cout << *(arr + i) << " ";
     }
 }
 
 int main()
 {
-    int returnFcn = *reverseArray(arr, sizeArr);
-    print(arr, sizeArr);
+    int *returnFcn = reverseArray(arr, sizeArr);
+    print(returnFcn, sizeArr);
 }
 
